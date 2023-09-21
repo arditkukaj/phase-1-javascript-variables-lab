ba0f62845eec7378231566f9bf18b4ea9e6555c9
arditkukaj <arditkukaj4@gmail.com>
2023-09-21 12:31:25 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+//define with const
+const companyName = 'Scuber';
+
+//define with let
+let mostProfitableNeighborhood = 'Chelsea';
+
+//define with let 
+let companyCeo = 'Susan Smith';
+
